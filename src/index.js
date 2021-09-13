97e77e4fce11d0aafe4a9fe4cbd296f576f0cc06
Vasily Kovnev <v.k95@yandex.ru>
2021-09-13 19:45:34 +0300

    feat: add chat functionality

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import cors from 'cors';
+import { Server } from 'socket.io';
+import path from 'path';
+import morgan from 'morgan';
+
+import ChatEvents from './events/chat';
+
+require('dotenv').config();
+
+const port = process.env.SERVER_PORT;
+const app = express();
+
+app
+  .use('/static', express.static(path.resolve(__dirname, 'public')))
+  .use(cors())
+  .use(morgan('dev'));
+
+const server = app.listen(port, () => {
+  console.log('Server run at ' + port);
+});
+
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('User connected', socket.id);
+
+  socket.on(ChatEvents.SEND_MESSAGE_FROM_CLIENT, (message) => {
+    io.emit(ChatEvents.GET_MESSAGE_FROM_SERVER, message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected', socket.id);
+  });
+});
